4f9f4d9b63c6d44b5e85efc4b902629fea89162c
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-09-28 22:05:22 -0300

    fix(windowSize): update inner model after a window resize event

--- internal/terminal/ui.go
+++ internal/terminal/ui.go
@@ -147,7 +147,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.viewport = newViewport(largePaddingStyle, msg)
-		return m, nil
 
 	case spinner.TickMsg:
 		m.spinner, cmd = m.spinner.Update(msg)
